1798a6305b52cde3cfacc445e278083183bb18e9
Psychac <52043937+Psychac@users.noreply.github.com>
2021-07-08 12:29:01 +0530

    Fixed solving without calibration

--- app.py
+++ app.py
@@ -17,18 +17,19 @@ app.config['SECRET_KEY'] = 'PuzzleMaster'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
 # limit upload size to 2mb
-app.config['MAX_CONTENT_LENGTH'] = 6 * 1024 * 1024 
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024 
 
 
 prominent_color_palette = {
-    'Red'   : [0, 0, 255],
-    'Orange': [0, 165, 255],
-    'Blue'  : [255, 0, 0],
-    'Green' : [0, 255, 0],
+    'Red'   : [185, 0, 0],
+    'Orange': [255, 89, 0],
+    'Blue'  : [0, 69, 173],
+    'Green' : [0, 155, 72],
     'White' : [255, 255, 255],
-    'Yellow': [0, 255, 255]
+    'Yellow': [255, 213, 0]
 }
 calibrated_colors = {}
+colors_per_square = {}
 notations = {}
 cubestring = {}
 
@@ -47,9 +48,9 @@ def setPallete():
      calibrated_colors[key] = value
    #print(calibrated_colors)
 
-   sidenotation = ast.literal_eval(data['side'])
-   for key,value in sidenotation.items():
-     notations[str(value)] = str(key)
+  #  sidenotation = ast.literal_eval(data['side'])
+  #  for key,value in sidenotation.items():
+  #    notations[str(value)] = str(key)
   #print(notations)
    #print(data)
    
@@ -101,17 +102,24 @@ def notation():
     else:
         color_pallete = prominent_color_palette
 
+    middle_square_color = rimg.data[5]
+    color = helpers.get_closest_color(middle_square_color,color_pallete)['color_name']
+    #print(color)
+    notations[color] = side[0]
+
     for key,value in rimg.data.items():
       color_name = helpers.get_closest_color(value,color_pallete)
       colors_for_side[key] = color_name['color_name']
-      sidenot = sidenot + notations[color_name['color_name']]
+      #sidenot = sidenot + notations[color_name['color_name']]
       color_rgb.append(value)
       color_short += color_name['color_name'][0]
 
-    cubestring[side] = sidenot
+    colors_per_square[side] = colors_for_side
+    #cubestring[side] = sidenot
     print(color_short)
     print(color_rgb)
-    print(cubestring)
+    print(notations)
+    #print(cubestring)
     #return jsonify({'status':True,'side':side,"color_rgb":color_rgb,'result':json.dumps(colors_for_side)})
     return jsonify({'status':True,"color_rgb":color_rgb,"color_name":color_short})
 
@@ -121,6 +129,18 @@ def notation():
 
 @app.route('/result', methods=['GET','POST'])
 def solve():
+    print(notations)
+    
+    for sidename in ("Up","Right","Front","Down","Left","Back"):
+      colors_per_side = colors_per_square[sidename]
+      #print(colors_per_side)
+      sidenot = ""
+      for key,value in colors_per_square[sidename].items():
+        sidenot = sidenot + notations[value]
+      cubestring[sidename] = sidenot
+      #print(sidenot)        
+      #print(colors_per_side)
+    
     resultstring = ""
 
     for sidename in ("Up","Right","Front","Down","Left","Back"):
